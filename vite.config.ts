3d04355684bd3a699442ae8d52c28686287ed9cb
Arnaud Lagutere <lagutere@hotmail.fr>
2025-04-30 17:08:01 +0200

    deploy le retour

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'node18', // Adapté à Railway
+    outDir: 'dist',
+    ssr: true, // Indique que c'est du server-side
+    rollupOptions: {
+      input: 'src/index.ts',
+      external: [
+        '.prisma/client',
+        'argon2',
+        'zod',
+        'hono',
+        '@prisma/client',
+        // ajoute ici les modules node à ne pas bundler
+      ]
+    }
+  }
 })

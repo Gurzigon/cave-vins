83b192200c5bc4752f399a5051e6376b39a6ac60
Arnaud Lagutere <lagutere@hotmail.fr>
2025-05-01 09:38:12 +0200

    Modif tsconfig et viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   build: {
     target: 'node18', // Adapté à Railway
     outDir: 'dist',
-    ssr: true, // Indique que c'est du server-side
+    ssr: true,
+    lib: {
+      entry: 'src/index.ts', // Point d'entrée
+      formats: ['cjs'], // Format de sortie en CommonJS pour Node.js
+    }, // Indique que c'est du server-side
     rollupOptions: {
       input: 'src/index.ts',
       external: [
